760278d3eb3cca28f43bf016f17581260374d1f6
Andraelity <Andraelity@gmail.com>
2021-12-12 12:50:02 -0800

    Seven commit

---
+++ Program/Program/MatricesBehavior.py
@@ -0,0 +1,27 @@
+
+
+#Como yo puedo organizar relaciones de un nivel mucho mas distinto, como yo puedo organizar posiciones las cuales motiven como yo puedo entender la realidad
+#Como yo puedo sentar las bases del desarrollo interno, como yo puedo sentir, el componente que me permite direccionar en la realidad al grado mas supremo
+#Como yo puedo sentar las bases para la construccion del desarrollo que se mueve en mi mente como yo puedo coordinar relaciones que me permiten crear mas accion.
+
+#Como yo puedo setear los comportamientos de la realidad capaces de responder al trabajo que define como nuestra mente se puede analizar a si misma, como la constante
+#Universal se vive en el desarrollo de los comportamientos como yo puedo sentar los procesos de mi narrativa, como yo puedo buscar las composiciones que definen que
+#Que realidad yo puedo aprender a genera, como yo puedo buscar el contenido con la totalidad.
+#Entre mas grafica mas rendimiento y entre mas rendimiento mas desarrollo y a mas desarrollo mas complejidad en las interacciones de los procesos que definen como la precision
+#de la mente se produce como los poderes internos del ser trabajan con tal de construir el proceso interno de la realidad.
+
+
+
+#Como yo puedo promover las siguientes interacciones como yo puedo procesar las relaciones de poder que motivan mi totalidad como yo puedo analizar las propiedades cualitativas
+#Entendiendo las propiedades del ahora.
+
+
+def consola(): 
+    for i in range(51):
+        
+    print()
+    print()
+    print()
+    
+    
+    return 
